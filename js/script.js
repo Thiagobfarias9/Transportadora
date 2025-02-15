83b5124705eb128200af15439230190acb9c4115
Thiago Barboza de Farias <thiagobfarias9@gmail.com>
2025-02-15 15:23:55 -0300

    Adicionada Verificação se o valor é NaN, e certas partes foram debugadas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('form')
+const body = document.querySelector('body')
 const fatorCubagem = 300
 const capacidade = 2000
  
@@ -12,60 +13,54 @@ form.addEventListener('submit', function(e){
     const altura = FormatarNumero(document.querySelector('input[name=altura]').value)
     const quantidade = FormatarNumero(document.querySelector('input[name=quantidade]').value)
 
-    console.log(largura)
-    console.log(profundidade)
-    console.log(altura)
-    console.log(quantidade)
-
     if(largura && profundidade && altura && quantidade)
     {
 
-        const cubagem = CalcularCubagem(largura,profundidade,altura)
-        const PesoPorCaixa = CalcularPeso(fatorCubagem, cubagem)
-     
-        const qtCaminhoes = CalcularQuantidadeCaminhoes(capacidade, PesoPorCaixa, quantidade)
-     
-     
-        const div = document.createElement('div')
-        div.id = "resposta"
-        const pPeso = document.createElement('p')
-        pPeso.textContent = `Peso da caixa: ${Math.round(PesoPorCaixa*100)/100} Kg`
-     
-        const pCubagem = document.createElement('p')
-        pCubagem.textContent = `Cubagem da caixa: ${cubagem}`
-     
-        const pPesoTotal = document.createElement('p')
-        pPesoTotal.textContent = `Peso total: ${Math.ceil(PesoPorCaixa*quantidade)} Kg`
-     
-        const pQtCaminhoes = document.createElement('p')
-        pQtCaminhoes.textContent = `Quantidade de Caminhões: ${qtCaminhoes}`
-    
-        div.appendChild(pPeso)
-        div.appendChild(pPesoTotal)
-        div.appendChild(pCubagem)
-        div.appendChild(pQtCaminhoes)
-    
-        if(document.getElementById("resposta")){
-            form.replaceChild(div, document.getElementById("resposta"))
-        }
-        else{
-            form.appendChild(div)
-        }
+            const cubagem = CalcularCubagem(largura,profundidade,altura)
+            const PesoPorCaixa = CalcularPeso(fatorCubagem, cubagem)
+         
+            const qtCaminhoes = CalcularQuantidadeCaminhoes(capacidade, PesoPorCaixa, quantidade)
+
+            console.log(cubagem)
+         
+            if(!cubagem || !PesoPorCaixa || !qtCaminhoes)
+            {
+                CriarTextoAviso("Digite um número valido")
+            }
+            else
+            {
+                const div = document.createElement('div')
+                div.id = "resposta"
+                const pPeso = document.createElement('p')
+                pPeso.textContent = `Peso da caixa: ${Math.round(PesoPorCaixa*100)/100} Kg`
+             
+                const pCubagem = document.createElement('p')
+                pCubagem.textContent = `Cubagem da caixa: ${cubagem}`
+             
+                const pPesoTotal = document.createElement('p')
+                pPesoTotal.textContent = `Peso total: ${Math.ceil(PesoPorCaixa*quantidade)} Kg`
+             
+                const pQtCaminhoes = document.createElement('p')
+                pQtCaminhoes.textContent = `Quantidade de Caminhões: ${qtCaminhoes}`
+            
+                div.appendChild(pPeso)
+                div.appendChild(pPesoTotal)
+                div.appendChild(pCubagem)
+                div.appendChild(pQtCaminhoes)
+            
+                if(document.getElementById("resposta")){
+                    form.replaceChild(div, document.getElementById("resposta"))
+                }
+                else{
+                    form.appendChild(div)
+                }
+
+            }
+
     }
     else
     {
-        const p = document.createElement("p")
-        p.textContent = "Algum dos campos está vazio"
-        p.style.color = "red"
-        p.style.animationName = "Desaparecer"
-        p.style.animationDelay = "2s"
-        p.style.animationDuration = "1s"
-        form.appendChild(p)
-        setTimeout( function(){
-            p.style.opacity = "0"
-            form.removeChild(p)
-        },3000
-        )
+        CriarTextoAviso("Algum dos campos está vazio")
     }
  
 
@@ -91,11 +86,40 @@ function CalcularQuantidadeCaminhoes(capacidade, peso, quantidade){
 }
 
 function FormatarNumero(numero){
-    if(numero.includes(",") != -1)
+    if(numero.includes(","))
     {
         return numero.replace(",",".")
     }
     else{
         return numero
     }
+}
+
+function CriarTextoAviso(text)
+{
+
+    const p = document.createElement("p")
+    p.id = "TextAviso"
+    p.textContent = text
+    p.style.color = "red"
+    p.style.animationName = "Desaparecer"
+    p.style.animationDelay = "3.5s"
+    p.style.animationDuration = "1s"
+    body.appendChild(p)
+    setTimeout( function(){
+        body.removeChild(p)
+    },4000
+    )
+
+    // if(!document.querySelector("TextAviso"))
+    // {
+        
+    // }
+    // else{
+    //     body.replaceChild(p,document.querySelector("TextAviso"))
+    // }
+
+
+    
+
 }
